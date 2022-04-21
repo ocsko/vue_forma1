d74541d670279c221d7edb9205abc9f7a4e0bba6
sz0hamtam <sz0hamtam@vasvari.org>
2022-04-21 08:34:59 +0200

    Fogadas oldal elkezdve.

---
+++ src/views/BetView.vue
@@ -0,0 +1,58 @@
+<template>
+  <h2>Fogadjon futamra!</h2>
+  <div>
+    <form @submit.prevent="kuldes">
+      <div>
+        <label>Kérem írja be a futamot, amelyikre fogadni szeretne:</label>
+        <input @change="change" type="text" placeholder="Futam neve" name="futam" v-model="futam"/>
+      </div>
+      <div>
+        <label>Kérem írja be a csapat nevét:</label>
+        <input @change="change" type="text" placeholder="Csapat neve" name="csapat" v-model="csapat"/>
+      </div>
+      <div>
+        <label>Kérem írja be a versenyző nevét:</label>
+        <input @change="change" type="text" placeholder="Versenyző neve" name="versenyzo" v-model="versenyzo"/>
+      </div>
+      <div>
+        <input type="submit" value="Fogadás" :disabled="isDisabled" />
+      </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data(){
+    return{
+      futam: '',
+      csapat:  '',
+      versenyzo: '',
+      isDisabled: true,
+    }
+  },
+  methods:{
+    change(){
+      if (this.futam == '' || this.csapat == '' || this.versenyzo == ''){
+        this.isDisabled = true; 
+      } else{
+        this.isDisabled = false;
+      }
+    },
+    kuldes(){
+      let url = 'http://localhost:5000/api/Bets'
+      let adat = ''
+      if (this.futam != '' && this.csapat != '' && this.versenyzo != ''){
+        axios.post(url, adat)
+        .then((resp) => {console.log(resp)})
+        
+      } 
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
