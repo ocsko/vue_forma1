b45367b39d6466b05b90eff03a7be7c4a51c61cb
Dani-PC <sz0moldan@vasvari.org>
2022-04-22 19:41:56 +0200

    HomePage kép megjelenik

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <h2>Üdvözöljük a Forma-1-GO weboldalán!</h2>
+    <h2>Üdvözöljük a Forma 1-GO weboldalán!</h2>
     <h3>Lépjen a fent található menü segítségével!</h3>
   </div>
-  <div >
+  <div class="card col-lg-12 col-md-6 col-sm-12 p-1" >
     <h4>Ha még nem tette,töltse le mobilos applikációnkat is!</h4>
     <a href="">Letöltés</a>
     <br>
-    <img src="desktop2.jpg" alt="desktop">
+    <img src="../assets/mobil_1.png" alt="desktop">
   </div>
 </template>
 
@@ -22,3 +22,8 @@ export default {
   }
 }
 </script>
+<style>
+.card{
+  margin-bottom: 50px !important;
+}
+</style>
