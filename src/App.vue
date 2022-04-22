f63b62ba54bfdd7ea7c0aaf331f9908dfee3b4e5
Dani-PC <sz0moldan@vasvari.org>
2022-04-22 18:37:03 +0200

    HomePage, kép még nem jelenik meg

--- src/App.vue
+++ src/App.vue
@@ -28,6 +28,6 @@ nav a {
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: #d12f1a;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h2>Üdvözöljük a Forma-1-GO weboldalán!</h2>
+    <h3>Lépjen a fent található menü segítségével!</h3>
+  </div>
+  <div >
+    <h4>Ha még nem tette,töltse le mobilos applikációnkat is!</h4>
+    <a href="">Letöltés</a>
+    <br>
+    <img src="desktop2.jpg" alt="desktop">
   </div>
 </template>
 
